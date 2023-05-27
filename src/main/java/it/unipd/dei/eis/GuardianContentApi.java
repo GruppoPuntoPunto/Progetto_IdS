8cb1037a75f12f0bad7d7cab5a9bd1a8e2745e88
luca-d <lucapiai98@proton.me>
2023-05-27 18:45:28 +0200

    Versione zero funzionante
    Il conteggio non era implementato correttamente. Ho aggiunto il supporto
    per scaricare la risposta json da Windows anche se non l'ho testata.
    Ho provato a implementare factory+singleton per gestire le sorgenti.

--- src/main/java/it/unipd/dei/eis/GuardianContentApi.java
+++ src/main/java/it/unipd/dei/eis/GuardianContentApi.java
@@ -2,12 +2,10 @@ package it.unipd.dei.eis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
-public class GuardianContentApi {
+public class GuardianContentApi implements Source {
     private static final String TARGET_URL = "https://content.guardianapis.com/search?show-fields=all";
     private final String apiKey;
     private final String OS; // per decidere quale script lanciare
@@ -17,41 +15,43 @@ public class GuardianContentApi {
         OS = System.getProperty("os.name").toLowerCase();
     }
 
-    public Response getContent() {
-        return getContent(null);
-    }
+    public Response getContent() { return getContent(null); }
 
     public Response getContent(final String query) {
-        // download risposta
-        String bashCmd = "curl " + "\"" + TARGET_URL;
+        String cmd = ""; // comando da eseguire
+
+        if (OS.equals("windows 10") || OS.equals("windows 11"))
+            cmd += "Invoke-WebRequest -Method GET -URI ";
+        else cmd += "curl ";
+
+        cmd += "\"" + TARGET_URL;
 
         if (query != null && !query.isEmpty())
-            bashCmd += "&q=" + query;
+            cmd += "&q=" + query;
 
-        bashCmd += "&api-key=" + apiKey + "\"" + " -o data.json";
+        cmd += "&api-key=" + apiKey + "\" "; 
+
+        if (OS.equals("windows 10") || OS.equals("windows 11"))
+            cmd += "-OutFile \"data.json\"";
+        else cmd += "-o data.json";
 
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "-c", bashCmd);
-            builder.redirectErrorStream(true);
-
-            // lancio il processo
-            Process process = builder.start();
+            // lancio un processo per eseguire il comando e aspetto che termini
+            Process process = Runtime.getRuntime().exec(cmd);
             process.waitFor();
-
-            // stampa l'output del comando bash
-            //BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            //String line;
-            //while ((line = reader.readLine()) != null)
-            //    System.out.println(line);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
 
-        // serializzo la risposta
         ObjectMapper mapper = new ObjectMapper();
         ResponseWrapper response = new ResponseWrapper();
         try { response = mapper.readValue(new File("data.json"), ResponseWrapper.class); }
         catch (IOException e) { System.out.println(e); }
         return response.getResponse();
+    }
+
+    public Article[] getArticles() {
+        // in questo modo non possiamo sfruttare le query
+        return getContent().getResults();
     }
 }
---
+++ src/main/java/it/unipd/dei/eis/Source.java
@@ -0,0 +1,5 @@
+package it.unipd.dei.eis;
+
+public interface Source {
+    Article[] getArticles();
+}
