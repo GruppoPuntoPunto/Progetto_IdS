4bbadca884fb538050938a38aee790549d08de7e
luca-d <lucapiai98@proton.me>
2023-06-11 15:26:31 +0200

    Aggiornato GuardianJSONSource e SourceFactory, modificata path file ouput ed input

--- src/main/java/it/unipd/dei/eis/GuardianJSONSource.java
+++ src/main/java/it/unipd/dei/eis/GuardianJSONSource.java
@@ -2,41 +2,48 @@ package it.unipd.dei.eis;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import java.io.File;
 import java.io.IOException;
 
 public class GuardianJSONSource implements Source {
-    private static final String TARGET_URL = "https://content.guardianapis.com/search?show-fields=all&page-size=200";
-    private final String apiKey;
+    private static final String TARGET_URL = "https://content.guardianapis.com/search?show-fields=all";
+    private File directory; // cartella in cui salvare le risposte json
     private final String OS; // per decidere quale script lanciare
+    private final String apiKey;
     private Article[] results;
 
-    public GuardianJSONSource(String apiKey) {
-        this.apiKey = apiKey;
+    public GuardianJSONSource(String apiKey, String directory) {
         this.OS = System.getProperty("os.name").toLowerCase();
+        this.directory = new File(directory);
+        if (!this.directory.exists())
+            this.directory.mkdirs(); // creo la cartella se non esiste
+        this.apiKey = apiKey;
         this.results = new ArticleJSON[0];
     }
 
-    //public Response getContent() { return getContent(null); }
+    private String getDirectoryPath() {
+        String p = "";
+        try { 
+            p = directory.getCanonicalPath();
+        }
+        catch (IOException e) { 
+            e.printStackTrace(); 
+        }
+        return p;
+    }
 
-    public void download() {
-        String command = ""; // comando da eseguire
-        boolean osIsWindows = OS.contains("windows");
-
-        // costruisco il comando per scaricare la risposta json
-        command += "curl -o data.json \"" + TARGET_URL;
-
-        //if (query != null && !query.isEmpty())
-        //    command += "&q=" + query;
-
-        command += "&api-key=" + apiKey + "\" ";
-
+    private void executeShellCommand(String command) {
         try {
             // creo il processo
             ProcessBuilder builder = null;
-            if (osIsWindows)
+            if (OS.contains("windows"))
                 builder = new ProcessBuilder("cmd.exe", "/c", command);
-            else  builder = new ProcessBuilder("bash", "-c", command);
+            else
+                builder = new ProcessBuilder("bash", "-c", command);
 
             // lancio il processo
             Process process = builder.start();
@@ -44,15 +51,50 @@ public class GuardianJSONSource implements Source {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+    }
 
-        // serializzo 
+    public void download() {
+        String dirPath = getDirectoryPath();
+
+        // creazione file json
+        for (int i = 1; i <= 5; i++) {
+            // path file in cui salvare la risposta
+            String filePath = dirPath + "/" + i + ".json";
+
+            // comando da eseguire da shell
+            StringBuilder buildCmd = new StringBuilder();
+            buildCmd.append("echo > " + filePath)
+                    .append(" && curl -o " + filePath + " ")
+                    .append('\"' + TARGET_URL)
+                    .append("&page-size=" + 200)
+                    .append("&page=" + i)
+                    .append("&api-key=" + apiKey + '\"');
+            String cmd = buildCmd.toString();
+
+            // eseguo il comando
+            executeShellCommand(cmd);
+        }
+
+        // deserializzo tutti gli articoli scaricati dalle risposte
         ObjectMapper mapper = new ObjectMapper();
         ResponseWrapper response = new ResponseWrapper();
-        try { response = mapper.readValue(new File("data.json"), ResponseWrapper.class); }
-        catch (IOException e) { e.printStackTrace(); }
 
-        // acquisisco gli articoli
-        this.results = response.getResponse().getResults();
+        // lista in cui salvo tutti gli articoli
+        List<Article> tmp = new ArrayList<>(); 
+
+        File[] jsonFiles = directory.listFiles();
+        if (jsonFiles != null) {
+            for (File f : jsonFiles) {
+                try { 
+                    response = mapper.readValue(f, ResponseWrapper.class); 
+                } catch (IOException e) { 
+                    e.printStackTrace(); 
+                }
+                tmp.addAll(Arrays.asList(response.getResponse().getResults()));
+            }
+        }
+
+        this.results = tmp.toArray(this.results);
     }
 
     public Article[] getArticles() {
--- src/main/java/it/unipd/dei/eis/SourceFactory.java
+++ src/main/java/it/unipd/dei/eis/SourceFactory.java
@@ -8,8 +8,8 @@ public class SourceFactory {
     private SourceFactory() { }
 
     public Source createSource(String sourceType, Object... args) {
-        if (sourceType.equals("GuardianJSONSource") && args[0] instanceof String)
-            return new GuardianJSONSource((String) args[0]);
+        if (sourceType.equals("GuardianJSONSource") && args[0] instanceof String && args[1] instanceof String)
+            return new GuardianJSONSource((String) args[0], (String) args[1]);
         else if (sourceType.equals("NewYorkTimesCSVSource") && args[0] instanceof FileReader)
             return new NewYorkTimesCSVSource((FileReader) args[0]);
         return null;
